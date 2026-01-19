e45e3d71db4cf7cdfd34268b568d7b7f75dd792b
Admkhzmi <admkhzmi@gmail.com>
2026-01-20 07:29:03 +0800

    Add Firebase configuration to config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+window.__SLC_FIREBASE_CONFIG__ = {
+  apiKey: "AIzaSyAGhkQhJYk4ZU-Fmn_sj3469toPTzrWbPE",
+  authDomain: "softcare-fc2d7.firebaseapp.com",
+  projectId: "softcare-fc2d7",
+  storageBucket: "softcare-fc2d7.firebasestorage.app",
+  messagingSenderId: "741838046763",
+  appId: "1:741838046763:web:2d8b49fb9e0dc6cd2ecc6e",
+  measurementId: "G-C3DEFN3RLB"
+};
