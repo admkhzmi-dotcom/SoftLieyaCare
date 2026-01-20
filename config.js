b8509f520f7dc55f0c23857849833f33d4ffeef2
Admkhzmi <admkhzmi@gmail.com>
2026-01-20 13:14:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
-window.__SLC_FIREBASE_CONFIG__ = {
+window.SLC = window.SLC || {};
+
+window.firebaseConfig = {
   apiKey: "AIzaSyAGhkQhJYk4ZU-Fmn_sj3469toPTzrWbPE",
   authDomain: "softcare-fc2d7.firebaseapp.com",
   projectId: "softcare-fc2d7",
@@ -7,3 +9,23 @@ window.__SLC_FIREBASE_CONFIG__ = {
   appId: "1:741838046763:web:2d8b49fb9e0dc6cd2ecc6e",
   measurementId: "G-C3DEFN3RLB"
 };
+
+(function initFirebase(){
+  function ready() {
+    return window.firebase && window.firebase.initializeApp;
+  }
+
+  const wait = setInterval(() => {
+    if (!ready()) return;
+    clearInterval(wait);
+
+    if (!firebase.apps.length) firebase.initializeApp(window.firebaseConfig);
+
+    window.SLC.auth = firebase.auth();
+    window.SLC.db = firebase.firestore();
+    window.SLC.ts = firebase.firestore.FieldValue.serverTimestamp;
+
+    // Slightly friendlier persistence behavior
+    window.SLC.auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL).catch(()=>{});
+  }, 50);
+})();
